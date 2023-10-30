f6194f33832e8f695879af52e413761b336ce4a6
RITINHHA <cassia.moura@academico.ifrn.edu.br>
2023-10-29 22:37:32 -0300

    PRONTOO

--- RitaPeoo/Agenda/streamlit/Scripts/activate.bat
+++ RitaPeoo/Agenda/streamlit/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Windows\System32\RitaPeoo\Agenda\streamlit
+set VIRTUAL_ENV=C:\RitaPeoo\agenda\streamlit
 
 if not defined PROMPT set PROMPT=$P$G
 
